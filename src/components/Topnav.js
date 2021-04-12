3e9cb4d6a2de98664f0cc05504ee8a7aeff8fc26
Peechanok <poppypang99@gmail.com>
2021-04-12 14:43:24 +0700

    add ยอดขาย เพิ่ม ฟิล จำนวน เพิ่ม ui login ,register

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -32,7 +32,7 @@ function Navigations() {
 
       <Navbar defaultActiveKey="/Product" collapseOnSelect toggleable="lg" expand='sm' bg='light' variant='light' >
 
-        <Navbar.Brand href="/Product"> <img
+        <Navbar.Brand href="/"> <img
           alt=""
           src={goods}
           width="30"
@@ -43,7 +43,7 @@ function Navigations() {
         <Navbar.Collapse id="basic-navbar-nav">
 
           <Nav justified className="ml-auto" >
-            <Nav.Link href="/Product">หน้าร้าน</Nav.Link>
+            <Nav.Link href="/">หน้าร้าน</Nav.Link>
             <Nav.Link href=""></Nav.Link>
 
             <Nav.Link href="/Warehouse">คลังสินค้า</Nav.Link>
