98fcc6a28247bd4ac6edc230c7af1d25a1be170a
Tanachot Chirakitsakul <42908486+cotver@users.noreply.github.com>
2021-05-13 16:37:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ db.sequelize.sync({ force: false }).then(() => {
 //   res.json({'test':'xxxx'});
 // });
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 
 
 app.use(function(req, res, next) {
@@ -43,11 +43,6 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
-
-
 
 //
 
